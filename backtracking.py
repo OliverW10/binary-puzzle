9a85ba0ec4f6f907a7243bfe34da6ae306e33f8e
oliverw10 <oliver@warrickfamily.com>
2021-03-24 14:16:08 +1100

    added comments

--- backtracking.py
+++ backtracking.py
@@ -13,7 +13,7 @@ test2 = np.array(
 	[[1, 0, 0, 1, 1, 0],
 	[0, 1, 1, 0, 0, 1],
 	[0, 1, 0, 1, 1, 0],
-	[1, 0, 0, 1, 0, 1],
+	[1, 0, 0, 1, 1, 0],
 	[0, 1, 1, 0, 1, 0],
 	[1, 0, 1, 0, 0, 1]])
 
@@ -59,35 +59,46 @@ def checkBoard(board):
 			vert = [board[i][j-1], board[i][j], board[i][j+1]]
 			if vert == threes[0] or vert == threes[1]:
 				return False
+
 	return True
 
 def findEmpty(board):
+	# finds the first square with a two from left to right top to bottom
 	allPos = np.where(board==2)
 	if len(allPos[1]) == 0:
 		return False
 	return (allPos[0][0], allPos[1][0])
 
 def branch(board):
+
+	# checks if we filled every square, meaning we found a solution
 	pos = findEmpty(board)
 	if pos == False:
 		return True, board
 
+	# sets the next undecided square to 0
 	board[pos[0], pos[1]] = 0
+	# checks if its valid (so far)
 	if checkBoard(board):
+		# if it is continue recursively branching down
 		out = branch(board)
-		if out[0] == True:
+		if out[0] == True: # found a solution in this branch
 			return True, out[1]
 
+	# sets the next undecided square to 1
 	board[pos[0], pos[1]] = 1
+	# checks if its valid (so far)
 	if checkBoard(board):
+		# if it is continue recursively branching down
 		out = branch(board)
-		if out[0] == True:
+		if out[0] == True: # found a solution in this branch
 			return True, out[1]
 
 	board[pos[0], pos[1]] = 2
-	return False, []
+	return False, [] # didnt find a solution in this branch
 
 def printBoard(board):
+	print(board)
 	for i in range(size):
 		for j in range(size):
 			print(board[i][j], end="")
